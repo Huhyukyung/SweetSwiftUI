8f33f0c4cc8203bac79f271ac8ba304dad851a79
Yukyung Huh <huhyukyung@gmail.com>
2023-08-19 21:52:38 +0900

    프로퍼티로 뷰 추출하기

--- FruitMart/Source/User Interface/Home/ProductRow.swift
+++ FruitMart/Source/User Interface/Home/ProductRow.swift
@@ -11,41 +11,8 @@ import SwiftUI
 struct ProductRow: View {
     var body: some View {
         HStack{
-            Image("apple")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 140)
-                .clipped()
-            VStack(alignment: .leading) {
-                Text("백설공주 사과")
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .padding(.bottom, 6)
-                
-                Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                
-                Spacer()
-                
-                HStack(spacing: 0) {
-                    Text("₩").font(.footnote)
-                    + Text("2100").font(.headline)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "heart")
-                        .imageScale(.large)
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                    
-                    Image(systemName: "cart")
-                        .foregroundColor(Color("peach"))
-                        .frame(width: 32, height: 32)
-                }
-            }
-            .padding([.leading, .bottom], 12)
-            .padding([.top, .trailing])
+            productImage
+            productDescription
         }
         .frame(height: 150)
         .background(Color.primary.colorInvert())
@@ -55,6 +22,51 @@ struct ProductRow: View {
     }
 }
 
+private extension ProductRow {
+    var productImage: some View {
+        Image("apple")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 140)
+            .clipped()
+    }
+    
+    var productDescription: some View {
+        VStack(alignment: .leading) {
+            Text("백설공주 사과")
+                .font(.headline)
+                .fontWeight(.medium)
+                .padding(.bottom, 6)
+            
+            Text("달콤한 맛이 좋은 과일의 여왕 사과. 독은 없고 꿀만 가득해요!")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+            
+            Spacer()
+            footerView
+        }
+        .padding([.leading, .bottom], 12)
+        .padding([.top, .trailing])
+    }
+    
+    var footerView: some View {
+        HStack(spacing: 0) {
+            Text("₩").font(.footnote)
+            + Text("2100").font(.headline)
+            
+            Spacer()
+            
+            Image(systemName: "heart")
+                .imageScale(.large)
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+            
+            Image(systemName: "cart")
+                .foregroundColor(Color("peach"))
+                .frame(width: 32, height: 32)
+        }
+    }
+}
 
 struct ProductRow_Previews: PreviewProvider {
     static var previews: some View {
